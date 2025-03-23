443ab409a5052b7978b15de722fa47fc6fa0bc59
Isha Khutafale <146433294+ishaKhutafale@users.noreply.github.com>
2025-03-23 11:41:51 +0530

    Delete campusguide directory

--- campusguide/navigation/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from navigation import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('display_directions/', views.display_directions, name='display_directions'),
-]
